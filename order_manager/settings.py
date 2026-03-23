63f923b4f9af3ffa416163665aea3fbd276fc84a
JohnHtun <kyawzawtun220589@gmail.com>
2026-03-23 11:18:04 +0630

    fix allowed hosts

--- order_manager/settings.py
+++ order_manager/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-ALLOWED_HOSTS = ['*']  # for now
+# ALLOWED_HOSTS = ['*']  # for now
+ALLOWED_HOSTS = ['order-manager-6.onrender.com']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
